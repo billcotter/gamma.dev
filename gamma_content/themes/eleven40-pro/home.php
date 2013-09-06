9f574f5b62e1e182c34e1c22de7cf09288572d39
bill cotter <billcotter@outlook.com>
2013-09-05 20:37:50 -0400

    added jetpack

--- gamma_content/themes/eleven40-pro/home.php
+++ gamma_content/themes/eleven40-pro/home.php
@@ -16,9 +16,9 @@ function eleven40_grid_loop_helper() {
 	if ( function_exists( 'genesis_grid_loop' ) ) {
 		genesis_grid_loop( array(
 			'features'				=> 1,
-			'feature_image_size'	=> 0,
+			'feature_image_size'	=> 1,
 			'feature_image_class' 	=> 'alignleft post-image',
-			'feature_content_limit' => 0,
+			'feature_content_limit' => 350,
 			'grid_image_size'		=> 'grid-featured',
 			'grid_image_class'		=> 'grid-featured',
 			'grid_content_limit' 	=> 250,
